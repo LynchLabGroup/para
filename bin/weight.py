66f867b12f38ccea7d222154b4f7eb6bbd1e0ea6
Rekyt <matthias.grenie@ens-lyon.fr>
2014-02-28 18:01:41 -0500

    Modified WeightSeq class, it can know retrieve motifs according to thresholds

--- bin/weight.py
+++ bin/weight.py
@@ -14,7 +14,7 @@ class WeightSeq(object):
 	seq - aligned sequence, capitals show determined motidfs
 	w - list of pairs of nt and their phylogenetic footpringtin coefficient
 	"""
-	def __init__(self,line,pred):
+	def __init__(self,line):
 		"""
 		line - Pass a sequence line of a StatAlign file
 		pred - pass name of pred file
@@ -29,19 +29,9 @@ class WeightSeq(object):
 
 		seq = line[1].strip("\n") #aligned sequence, capitals are motifs
 
-		weighted = []
-		with open(pred,"r") as f:
-			for c in seq:
-				num = 0.0
-				if c != "-":
-					num = f.readline().rstrip("\n") #advances line
-					num = float(num) #convert string line to float coefficient
-				weighted.append([c,num])
-
 		#Initialize object
 		self._name = name
 		self._seq = seq
-		self._w = weighted
 
 	def __str__(self):
 		"""
@@ -51,5 +41,34 @@ class WeightSeq(object):
 
 	def __repr__(self):
 		return self.__str__()
+
+	def weight(self,pred):
+		"""
+		Function to weight the sequence according to pred scores in pred file. The pred file must be a column of each pred score.
+		"""
+		weighted = []
+		with open(pred,"r") as f:
+			for c in self._seq:
+				num = 0.0
+				if c != "-":
+					num = f.readline().rstrip("\n") #advances line
+					num = float(num) #convert string line to float coefficient
+				weighted.append([c,num])
+
+		self._w = weighted
+
+	def motifs(self,thre):
+		"""
+		If sequence has been weighted, shows all positions where prediction is higher than threshold
+		"""
+		if self._w == []:
+			return "You first have to weight the sequence using prediction scores !"
+		else:
+			for ind,k in enumerate(self._w):
+				nt = k[0] #nucleotide
+				w = k[1] #phylogenetic prediction
+				if w > thre:
+					print "index: {} base: {} mpd: {}".format(ind+1,nt,w)
+
 
 
