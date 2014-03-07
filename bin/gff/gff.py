241061ff1921e6fb976727464e74f6f054d5095b
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-07 12:11:57 -0500

    GFF parser is now fully functionnal and can write a fasta out of retrieved sequences

--- bin/gff/gff.py
+++ bin/gff/gff.py
@@ -5,24 +5,36 @@
 
 # Imports
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 from BCBio import GFF
-import time
 
-def retrievePos(geneid,gfffile,fastafile,length=100):
+def retrievePos(geneids,gfffile,fastafile,length=100):
 	"""
-	This function takes a gene id corresponding to a specific gene in a GFF file, and a length (default = 100nt), it retrieves the start and end position and verify the eventual overlap
+	DESCRIPTION:
+
+	Take a list of gene ids, and retrieve the upstream sequence (default = 100nt) from fastafile, verifying in gfffile that there is no overlap. Return a list of genes, locations and sequences.
+
+	USAGE:
+
+	geneids -- list of gene ids in gfffile
+	gfffile -- a GFF file
+	fastafile -- a Fasta file
+	length (optional) -- length of the upstream sequence to extract
 	"""
 	rec = [] # list of records of each entry in gff file
 
-	# GFF file parser
+	# GFF file parser, puts GFF file in memory
 	with open(gfffile,"r") as f:
 		print "Parsing file {}...".format(gfffile)
 		for line in GFF.parse(f):
 			rec.append(line) # scrap each id in GFF file and put it in a list
 
-	# Looping into content to identify start and end
+	interest = [] # list of coordinates of gene of interest
+
+	# Looping into content to identify characteristics of gene of interest
 	postable = [] # table of start, end, strand and sequence data on each genes
-	print "Extracting gene of interest..."
+	print "Extracting genes of interest..."
 	for r in rec:
 		feat = r.features # features of each record
 		for f in feat: # looping in all features (genes) of each sequence (scaffold)
@@ -31,58 +43,49 @@ def retrievePos(geneid,gfffile,fastafile,length=100):
 			end = f.sub_features[0].location.end.position 
 			strand = f.sub_features[0].location.strand
 
-			# Retrieves data for gene of interest
-			if name == geneid:
-				seqid = r.id
-				genestart = start
-				geneend = end
-				genestrand = strand
+			# Retrieves data from genes of interest
+			for g in geneids:
+				if name == g:
+					interest.append([start,end,strand,g,r.id])
 			
 			postable.append([start,end,strand,r.id]) #constructing a position matrix
 
-
-	# Verifying there is no overlap between upstream sequence you to extract and anothergenes
-	
-	# Remove positions of sequences differents from sequences of the gene of interest
-	I = set()
-	for pos in postable:
-		if pos[3] != seqid:
-			I.add(postable.index(pos))
-	for i in sorted(I,reverse=True):
-		del postable[i]
-			
-	
-	postable.sort() #sort for starting position
-
-	# Retrieves start and end position to extract, while verifying there is overlapping genes
+	# Retrieves start and end position to extract, while verifying there is no overlapping genes
 	upstream = []
 	print "Retrieving upstream locations..."
-	if genestrand == 1:
-		extract = genestart - length # beginning of the extract
-		if extract <= 0: # we don't want to extract negative bases!
-			extract = 1
-		for pos in postable:
-			if pos[1] in range(extract,genestart):# if the gene is overlapping
-				extract = pos[1]+1
-		upstream = [extract,genestart,genestrand] # gives the sequence you want to extract upstream of the gene
-	elif genestrand == -1:
-		extract = geneend + length # end of the extract
-		seqlen = fastalen(fastafile,seqid)
-		if extract >= fastalen : #if extract is over the sequence
-			extract = seqlen
-		for pos in postable:
-			if pos[0] in range(geneend+1,extract+1):
-				extract = pos[0]
-		upstream = [geneend+1,extract,genestrand]
+	for i in interest:
+		start = i[0] # natural beginning of the gene (begins with position 1)
+		end = i[1] # natural ending
+		strand = i[2] # strand 1: plus -1: minus
+		gene = i[3] # gene id
+		seqid = i[4] # sequence id on which gene is located
+		extract = 0
+		if strand == 1:
+			extract = start - length # beginning of the extract
+			if extract <= 0: # we don't want to extract negative bases!
+				extract = 1
+			for pos in postable:
+				if pos[-1] == seqid and pos[1] in range(extract,start):# if the gene is overlapping on the same scaffold
+					extract = pos[1]+1
+			upstream.append([extract,start,strand,gene,seqid])# gives the sequence you want to extract upstream of the gene
+		elif strand == -1:
+			extract = end + length # end of the extract
+			seqlen = fastalen(fastafile,seqid)
+			if extract >= fastalen : #if extract is over the sequence
+				extract = seqlen
+			for pos in postable:
+				if pos[-1] == seqid and pos[0] in range(end+1,extract+1):
+					extract = pos[0]
+			upstream.append([end+1,extract,strand,gene,seqid])
 
-	upstream.append(geneid,seqid) #add infos to use blastdbcmd afterwards + take out the sequence
+	print "Retrieving sequences..."
+	upstream = retrieveSeq(fastafile,upstream)
 	
 	return upstream
 
 def fastalen(fastafile,seqid):
-	"""
-	This function returns the length of fasta sequence using the sequence id and the fastafile
-	"""
+	"""Return length of sequence seqid in fastafile."""
+
 	length = 0
 
 	# Find the length in fastafile of seqid
@@ -97,9 +100,7 @@ def fastalen(fastafile,seqid):
 
 
 def retrieveSeq(fastafile,uplist):
-	"""
-	Using a list of retrievePos returns and a fastafile, gives back the extracted sequence.
-	"""
+	"""Take retrievePos return and return the same list with sequences appended."""
 
 	records = [] # list of sequences
 
@@ -118,3 +119,34 @@ def retrieveSeq(fastafile,uplist):
 
 	return uplist
 
+def writeFasta(filename,upseqs):
+	"""Write a fastafile from upstream sequences list returned by retrievePos."""
+
+	records = []
+
+	for u in upseqs:
+		start = u[0]
+		end = u[1]
+		strand = u[2]
+		gene = u[3] # gene name
+		seqid = u[4] # name of scaffold from which the gene is extracted
+		seq = u[5] # Seq object
+
+		feat = SeqFeature(FeatureLocation(start,end),strand=strand)
+		
+		if strand == 1:
+			strand = "+"
+		elif strand == -1:
+			strand = "-"
+		else:
+			strand = "?"
+
+		ident = seqid+"|"+gene+"|"+str(start)+"-"+str(end)+"|"+strand
+
+		rec = SeqRecord(seq,id=ident,name=gene,features=[feat],description="")
+
+		records.append(rec)
+
+	print "Writing file..."
+	SeqIO.write(records,filename,"fasta")
+	print "File {} written !".format(filename)
