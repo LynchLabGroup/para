d24be8638c530cb74bfabc65b0775ddc8efe276c
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-21 13:33:43 -0400

    Moved alignments results and add a script to automate alignment

--- bin/scripts/multialign.sh
+++ bin/scripts/multialign.sh
@@ -4,5 +4,5 @@
 for FILE in `ls para/data/families/WGD1/CDS/`;
 do
 FILEBASENAME=$(echo $FILE | cut -d. -f1)
-./muscle3.8.31_i86darwin64 -in para/data/families/WGD1/CDS/$FILE -out para/results/multialign/$FILEBASENAMEalign.fasta
+./muscle3.8.31_i86darwin64 < para/data/families/WGD1/CDS/$FILE > para/data/families/WGD1/CDS/align-$FILE
 done
