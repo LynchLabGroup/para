47789944e0d05c9a717749466d145193a66bedb4
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-21 14:33:07 -0400

    Modified script to log outputs

--- bin/scripts/multialign.sh
+++ bin/scripts/multialign.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script for automatic multialignment using MUSCLE
+# need to have muscle executable at the root!
 
 for FILE in `ls para/data/families/WGD1/CDS/`;
 do
 FILEBASENAME=$(echo $FILE | cut -d. -f1)
-./muscle3.8.31_i86darwin64 < para/data/families/WGD1/CDS/$FILE > para/results/multialign/align-$FILE -log outalign-$FILEBASENAME -stable
+./muscle3.8.31_i86darwin64 < para/data/families/WGD1/CDS/$FILE > para/results/multialign/align-$FILE -log outalign-$FILEBASENAME
 done
