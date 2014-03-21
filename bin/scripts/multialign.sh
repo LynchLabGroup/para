c8a8f0e454779c6bc47abb4c6daa603d27333ae5
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-21 14:15:56 -0400

    Added in alignscript order conservation

--- bin/scripts/multialign.sh
+++ bin/scripts/multialign.sh
@@ -4,5 +4,5 @@
 for FILE in `ls para/data/families/WGD1/CDS/`;
 do
 FILEBASENAME=$(echo $FILE | cut -d. -f1)
-./muscle3.8.31_i86darwin64 < para/data/families/WGD1/CDS/$FILE > para/results/multialign/align-$FILE -log outalign-$FILEBASENAME
+./muscle3.8.31_i86darwin64 < para/data/families/WGD1/CDS/$FILE > para/results/multialign/align-$FILE -log outalign-$FILEBASENAME -stable
 done
