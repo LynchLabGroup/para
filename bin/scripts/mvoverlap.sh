be1a282cb301fd2f4e07961652a415a2d11da174
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-25 10:48:45 -0400

    Modified mvoverlap script for specific purpose

--- bin/scripts/mvoverlap.sh
+++ bin/scripts/mvoverlap.sh
@@ -5,7 +5,7 @@ echo "Moving overlapped families..."
 for FILE in `cut -f 1 data/families/WGD2/upstream/overlap.txt`;
 do
 
-mv data/families/WGD2/upstream/$FILE.fasta data/families/WGD2/upstream/discarded/$FILE.fasta
+mv data/families/WGD2/CDS/nt/$FILE.fasta data/families/WGD2/CDS/nt/discarded/$FILE.fasta
 echo "Moved file $FILE..."
 done
 echo "Done."
