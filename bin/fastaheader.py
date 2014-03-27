924faee9d3054ae9b05b18c95d901845ce9be967
Rekyt <matthias.grenie@ens-lyon.fr>
2014-03-27 10:57:27 -0400

    Modified fasta header to write phylip files

--- bin/fastaheader.py
+++ bin/fastaheader.py
@@ -31,5 +31,22 @@ def rewrite_phylip(filename,output=None):
 			rec_out.append(rec)
 	
 	print "Writing file {}".format(output)
-	SeqIO.write(rec_out,output,"phylip")
+	SeqIO.write(rec_out,output,"phylip-relaxed")
+	print "Done."
+
+def rewrite_fasta(filename,output=None):
+	"""Take a fasta file and rewrite a fasta file with shortened name."""
+	if output == None:
+		output = filename.split(".")[0] + "rw.fasta"
+
+	rec_out = []
+	with open(filename,"r") as f:
+		for record in SeqIO.parse(f,"fasta"):
+			newname = shorten_name(record)
+
+			rec = SeqRecord(record.seq,id=newname,name=newname,description="")
+			rec_out.append(rec)
+	
+	print "Writing file {}".format(output)
+	SeqIO.write(rec_out,output,"fasta")
 	print "Done."
