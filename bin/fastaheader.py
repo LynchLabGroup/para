95b05d4fc5c562f6215b072a2c9dcb10b7fdf6ca
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-01 09:53:58 -0400

    Changed header modifier

--- bin/fastaheader.py
+++ bin/fastaheader.py
@@ -2,18 +2,22 @@
 # -*- coding: utf-8 -*-
 
 # This scripts rewrite Fasta header to have sequence name in first place
+import argparse
 
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
-def shorten_name(seqrecord):
+def shorten_name(seqrecord,char):
 	"""This function return shortened name of given sequence."""
 
 	name = seqrecord.description
 
-	name = name.split("_|_")
+	name = name.split(char)
 
 	ident = name[1]
+
+	if len(ident) > 10:
+		ident = ident[0:10]
 
 	return ident
 
@@ -34,15 +38,17 @@ def rewrite_phylip(filename,output=None):
 	SeqIO.write(rec_out,output,"phylip-relaxed")
 	print "Done."
 
-def rewrite_fasta(filename,output=None):
+def rewrite_fasta(filename,output=None,char=None):
 	"""Take a fasta file and rewrite a fasta file with shortened name."""
 	if output == None:
 		output = filename.split(".")[0] + "rw.fasta"
+	if char == None:
+		char = " | "
 
 	rec_out = []
 	with open(filename,"r") as f:
 		for record in SeqIO.parse(f,"fasta"):
-			newname = shorten_name(record)
+			newname = shorten_name(record,char)
 
 			rec = SeqRecord(record.seq,id=newname,name=newname,description="")
 			rec_out.append(rec)
@@ -50,3 +56,18 @@ def rewrite_fasta(filename,output=None):
 	print "Writing file {}".format(output)
 	SeqIO.write(rec_out,output,"fasta")
 	print "Done."
+
+if __name__ == "__main__":
+	### Command-line Parser ###
+
+	parser = argparse.ArgumentParser(description="Program to simplify sequence name in fasta file.")
+
+	parser.add_argument("fasta", help=".fasta file")
+
+	parser.add_argument("char", help="char that separate fasta header")
+
+	parser.add_argument("out", help="output file fasta format")
+
+	args = parser.parse_args()
+
+	rewrite_fasta(args.fasta,args.out,args.char)
