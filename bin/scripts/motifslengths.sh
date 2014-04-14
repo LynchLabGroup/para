3ac267bb635611aaa8ae10dc4dad99eefc848b7c
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-14 10:41:13 -0400

    Modified code to have command to find families with no motifs

--- bin/scripts/motifslengths.sh
+++ bin/scripts/motifslengths.sh
@@ -1,1 +1,4 @@
 ls WGD2ANC0*/*motifs | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' >> motifs.lengths
+
+# Command to look at how many families had no motifs found
+# awk '{if($1 == 2) print $0}' results/lengthsmotifsfiles.txt | wc -l
