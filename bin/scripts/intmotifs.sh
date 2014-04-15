7188921923ce329e31b8da9a19f67da21b7551fc
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-15 11:21:25 -0400

    Modified script to concatenate intmotifs and motiflengths scripts

--- bin/scripts/intmotifs.sh
+++ bin/scripts/intmotifs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script to look at how many interesting motifs have been found
 # USAGE:
-# intmotifs lengthsmotifsfile > intmotifsfile
+# intmotifs > intmotifsfile
 
-for FAM in `awk '{if ($1 > 2) print $2}' $1`;
+for FAM in `ls WGD2ANC0*/*motifs | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' | awk '{if($1 > 2) print $2}'`;
 do
 	echo $FAM
 	awk '{if($11 != "" && $11 != 0.0) print $11}' $FAM/$FAM.motifs
