205287c5cd02cfea5b219a1dc46894ad21d69213
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-15 11:46:32 -0400

    Added automatic redirection

--- bin/scripts/intmotifs.sh
+++ bin/scripts/intmotifs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Script to look at how many interesting motifs have been found
 # USAGE:
-# intmotifs > intmotifsfile
+# intmotifs file_to_write
 
 for FAM in `ls WGD2ANC0*/*motifs | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' | awk '{if($1 > 2) print $2}'`;
 do
 	echo $FAM
 	awk '{if($11 != "" && $11 != 0.0) print $11}' $FAM/$FAM.motifs
-done
+done < $1
