552feb1f68e702793d1c5aebbbf076d3393437df
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-16 12:20:43 -0400

    Added header to intmotifs outputs

--- bin/scripts/intmotifs.sh
+++ bin/scripts/intmotifs.sh
@@ -6,14 +6,20 @@
 temp=$(for FAM in `ls WGD2ANC0*/*motifs | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' | awk '{if($1 > 2) print $2}'`;
 do
 	echo $FAM
-	awk '{if($11 != "" && $11 != 0.0) print $11}' $FAM/$FAM.motifs
+	awk '{if($11 != "" && $11 != 0.0) print $3"\t"$11}' $FAM/$FAM.motifs
 done)
 
 echo "$temp" | head
 
+i=0
 echo "Writing file $1..."
 while read -r LINE
 do	
+	if [ $i == 0]
+	then
+		i=1
+		echo -e "family\tdistance\tsize"
+
 	if [[ $LINE == *WGD* ]]; then
 		NAME="$LINE"
 	else
