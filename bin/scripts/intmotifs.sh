537f286cbbf849e946cbc3b015a1f659209e8e32
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-30 12:54:06 -0400

    Updated script to avoid looking into meme's outputs

--- bin/scripts/intmotifs.sh
+++ bin/scripts/intmotifs.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+ #!/bin/bash
 # Script to look at how many interesting motifs have been found
 # USAGE:
 # intmotifs file_to_write
 
-temp=$(for FAM in `ls WGD2ANC0*/*motifs | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' | awk '{if($1 > 2) print $2}'`;
+temp=$(for FAM in `ls WGD2ANC0*/*motifs | grep -v "meme" | xargs wc -l | awk '{sub(/.WGD2ANC[0-9]*.motifs/,"")}1 && !/total/' | awk '{if($1 > 2) print $2}'`;
 do
 	echo $FAM
 	awk '{if($11 != "" && $11 != 0.0) print $3"\t"$11"\t"$7"\t"$9}' $FAM/$FAM.motifs
