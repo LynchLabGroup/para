c823024fd575719181cd20ea8c75914178785a60
Rekyt <matthias.grenie@ens-lyon.fr>
2014-04-30 17:37:12 -0400

    Changed tidyup script to be functionnal

--- bin/scripts/tidyup.sh
+++ bin/scripts/tidyup.sh
@@ -8,7 +8,7 @@ echo $d
 
 TARGET=$1
 
-find $TARGET -d 1 -name "*.nt[1-3]*_ali.fasta" -exec rm {} \;
-find $TARGET -d 1 -name "*.aa*" -exec rm {} \;
-find $TARGET -d 1 -name "*.html" -exec rm {} \;
-find $TARGET -d 1 -name "*.muscle.log" -exec rm {} \;
+find $TARGET -name "*.nt[1-3]*_ali.fasta" -delete
+find $TARGET -name "*.aa*" -delete
+find $TARGET -name "*.html" -delete
+find $TARGET -name "*.muscle.log" -delete
