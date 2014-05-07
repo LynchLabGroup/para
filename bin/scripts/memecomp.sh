0ab8a30c27fc5e90fb5dc4d2b354736d18b8f23c
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-07 17:22:28 -0400

    Changed script and added results of may 7th

--- bin/scripts/memecomp.sh
+++ bin/scripts/memecomp.sh
@@ -2,10 +2,11 @@
 # Script to use meme on every families 
 # USAGE:
 # memecomp.sh
-for FAM in `ls results/WGD2ANC0005*/*fasta | grep "WGD2ANC[0-9]\{5\}\.fasta" | cut -d. -f1`;
+for FAM in `ls results/WGD2ANC*/*fasta | grep "WGD2ANC[0-9]\{5\}\.fasta" | cut -d. -f1`;
 do
-
-meme $FAM.fasta -minw 4 -nmotifs 5 -dna -text> $FAM.meme.motifs
-python bin/bigfoot/memecomp.py $FAM.motifs $FAM.meme.motifs -t 0.9 -e 0.001 -o $FAM.comp
-
+if [ ! -f $FAM.meme.motifs ]
+then
+	meme $FAM.fasta -minw 4 -nmotifs 5 -dna -text> $FAM.meme.motifs
+	python bin/bigfoot/memecomp.py $FAM.motifs $FAM.meme.motifs -t 0.9 -e 0.001 -o $FAM.comp
+fi
 done
