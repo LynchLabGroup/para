cde6ddddf1242945c75f8f9ceb27f3a1e27daf24
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-11 15:58:03 -0400

    Modified syntax error in test code

--- test_motifs.py
+++ test_motifs.py
@@ -3,7 +3,7 @@
 # Test code to manage modifications for motif search
 
 from bin.bigfoot import parser
-s = 'results/WGD2ANC00051/WGD2ANC00051.fasta.'
+s = 'results/WGD2ANC00051/WGD2ANC00051.fasta'
 p = parser.SeqParser(s+".mpd",s+".pred")
 p.parse()
 p._parse[0].weight(s+".pred",s+".mpd")
