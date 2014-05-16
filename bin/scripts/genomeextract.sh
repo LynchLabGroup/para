ef97ad9b9ea1e6c2e194761ced6c991c9e08aeaa
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-16 19:39:12 -0400

    Corrected directory name error

--- bin/scripts/genomeextract.sh
+++ bin/scripts/genomeextract.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 # Extract all found motifs in all species
 # USAGE:
 # genomeextract.sh
@@ -15,9 +15,9 @@ extract(){
 
 while read -r line; 
 do
-    set -x 
     fields=(${line//\/})  # Splitting line in array
     echo ${fields[0]}
+    # To solve non existing files problem
 
-    extract ${fields[1]} results/motif_extraction/${fields[0]}bi data/biaurelia/biaurelia_V1-4_annotation_v1.gff3 data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
+    extract ${fields[1]} results/motifs_extraction/${fields[0]}bi data/biaurelia/biaurelia_V1-4_annotation_v1.gff3 data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
 done < <(awk '{print $1$2, $9}' results/15may14consensuscomp.txt | head)
