913d1656e2baebe7904e19a3353506667c5b8baf
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-17 22:39:49 -0400

    Added string replacement for outputs

--- bin/scripts/genomeextract.sh
+++ bin/scripts/genomeextract.sh
@@ -9,18 +9,22 @@ extract(){
     local gff=$3
     local assembly=$4
 
-    /N/u/jgout/Quarry/bin/extractGenesWithExactMotif $gff GFF Parent $assembly $motif 250 > $name.res 2> $name.log
+    /N/u/jgout/Quarry/bin/extractGenesWithExactMotif $gff GFF Parent $assembly 250 $motif > $name.res 2> $name.log
 
 }
 
 while read -r line; 
 do
-    fields=(${line//\/})  # Splitting line in array
-    echo ${fields[0]}
-    # To solve non existing files problem
-
-    extract ${fields[1]} results/motifs_extraction/${fields[0]}bi data/biaurelia/biaurelia_cds.gff data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
-    extract ${fields[1]} results/motifs_extraction/${fields[0]}ca data/caudatum/caudatum_cds.gff data/caudatum/caudatum_43c3d_assembly_v1.fasta
-    extract ${fields[1]} results/motifs_extraction/${fields[0]}sex data/sexaurelia/sexaurelia_cds.gff data/sexaurelia/sexaurelia_AZ8-4_assembly_v1.fasta
-    extract ${fields[1]} results/motifs_extraction/${fields[0]}bi data/tetraurelia/tetraurelia_cds.gff data/tetraurelia/ptetraurelia_mac_51.fa
-done < <(awk '{print $1$2, $9}' results/15may14consensuscomp.txt | head)
+	set -xv
+	fields=(${line//\/})  # Splitting line in array
+	echo ${fields[0]}
+	
+	extract ${fields[1]} results/motifs_extraction/${fields[0]}bi data/biaurelia/biaurelia_cds.gff data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
+	sed -e 's/./G/6' results/motifs_extraction/${fields[0]}bi.res	
+	extract ${fields[1]} results/motifs_extraction/${fields[0]}ca data/caudatum/caudatum_cds.gff data/caudatum/caudatum_43c3d_assembly_v1.fasta
+	sed -e 's/./G/6' results/motifs_extraction/${fields[0]}ca.res
+	extract ${fields[1]} results/motifs_extraction/${fields[0]}sex data/sexaurelia/sexaurelia_cds.gff data/sexaurelia/sexaurelia_AZ8-4_assembly_v1.fasta
+	sed -e 's/./G/7' results/motifs_extraction/${fields[0]}sex.res
+	extract ${fields[1]} results/motifs_extraction/${fields[0]}tet data/tetraurelia/tetraurelia_cds.gff data/tetraurelia/ptetraurelia_mac_51.fa
+	sed -e 's/./G/8' results/motifs_extraction/${fields[0]}tet.res
+done < <(awk '{print $1$2, $9}' results/15may14consensuscomp.txt | head )
