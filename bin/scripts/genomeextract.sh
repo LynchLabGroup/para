09758f4cd591d775080e232015f030cfd0206e3b
Rekyt <matthias.grenie@ens-lyon.fr>
2014-05-27 00:02:48 -0400

    Removed replacement

--- bin/scripts/genomeextract.sh
+++ bin/scripts/genomeextract.sh
@@ -17,17 +17,17 @@ LOCATION="results/motifs_extraction/"
 FILE="results/26may14uniquememe.txt"
 NAME="26may14MEMEMotifs"
 
-extract $FILE $LOCATION$NAMEbi data/biaurelia/biaurelia_cds.gff data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
-sed -e 's/./G/6' $LOCATION$NAMEbi.res > $LOCATION$NAMEbi.res
+extract $FILE $LOCATION$NAME.bi data/biaurelia/biaurelia_cds.gff data/biaurelia/biaurelia_V1-4_assembly_v1.fasta
+#sed -e 's/./G/6' $LOCATION$NAME.bi.res > $LOCATION$NAME.bi.res
 
-extract ${fields[1]} $LOCATION$NAMEca data/caudatum/caudatum_cds.gff data/caudatum/caudatum_43c3d_assembly_v1.fasta
-sed -e 's/./G/6' $LOCATION$NAMEca.res > $LOCATION$NAMEca.res
+extract $FILE $LOCATION$NAME.ca data/caudatum/caudatum_cds.gff data/caudatum/caudatum_43c3d_assembly_v1.fasta
+#sed -e 's/./G/6' $LOCATION$NAME.ca.res > $LOCATION$NAME.ca.res
 
-extract ${fields[1]} $LOCATION$NAMEsex data/sexaurelia/sexaurelia_cds.gff data/sexaurelia/sexaurelia_AZ8-4_assembly_v1.fasta
-sed -e 's/./G/7' $LOCATION$NAMEsex.res > $LOCATION$NAMEsex.res
+extract $FILE $LOCATION$NAME.sex data/sexaurelia/sexaurelia_cds.gff data/sexaurelia/sexaurelia_AZ8-4_assembly_v1.fasta
+#sed -e 's/./G/7' $LOCATION$NAME.sex.res > $LOCATION$NAME.sex.res
 
-extract ${fields[1]} $LOCATION$NAMEtet data/tetraurelia/tetraurelia_cds.gff data/tetraurelia/ptetraurelia_mac_51.fa
-sed -e 's/./G/8' $LOCATION$NAMEtet.res > $LOCATION$NAMEtet.res
+extract $FILE $LOCATION$NAME.tet data/tetraurelia/tetraurelia_cds.gff data/tetraurelia/ptetraurelia_mac_51.fa
+#sed -e 's/./G/8' $LOCATION$NAME.tet.res > $LOCATION$NAME.tet.res
 
 # Code for BigFoot motifs
 # <(awk '{print $1$2, $9}' results/15may14consensuscomp.txt)
