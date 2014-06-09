8cffa1c120be026ac7f2c9aba09db8d1b6d87a94
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-09 16:55:01 -0400

    Functionnal version of Snakefile with python2

--- Snakefile.py
+++ Snakefile.py
@@ -3,11 +3,11 @@
 import os
 import glob
 RES = [os.path.splitext(famfile)[0]+".comp" for famfile in glob.glob("results/WGD2ANC0000*/WGD2ANC0[0-9][0-9][0-9][0-9].motifs")]
-
+EVAL = 0.001
 rule all:
     input: RES
 
 rule comp:
     input: "{family}.motifs", "{family}.meme.motifs"
     output: "{family}.comp"
-    shell: "echo {input} && echo {output}"
+    shell: "python2 bin/bigfoot/memecomp.py {input} -e {EVAL} -o {output}"
