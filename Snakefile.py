919c9dac89062a73d6b54360e7665c0473c22117
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-09 22:01:24 -0400

    Added prefix function to align CDS

--- Snakefile.py
+++ Snakefile.py
@@ -71,7 +71,6 @@ rule transform_CDS_header:
 rule align_CDS:
     """Aligning CDSs"""
     input: "{family}.fasta"
+    params: prefix="{family}CDS"
     output: "{family}CDS.nt_ali.fasta"
-    run:
-        outfile = output.split(".")[0]
-        shell("perl bin/scripts/translatorx_vLocal.pl -c 6 -i {input} -o {outfile}")
+    shell: "perl bin/scripts/translatorx_vLocal.pl -c 6 -i {input} -o {params.prefix}"
