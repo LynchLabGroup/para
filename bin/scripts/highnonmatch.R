3a128ad653fe5bd276e7aa0fb8afcf8ebe9a14ac
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-10 15:11:03 -0400

    Changed script to add function

--- bin/scripts/highnonmatch.R
+++ bin/scripts/highnonmatch.R
@@ -4,20 +4,25 @@
 library(seqinr)
 
 # Data loading
-exp = read.table("data/tetraurelia_RNAseq_xp.tab", h=T) # Expression data
+exp = read.table("data/tetraurelia_RNAseq_xp.tab", h=TRUE) # Expression data
 ribo.seqs = read.fasta("../ribo.upstream.fa") # Ribosomal upstream sequences
 
+high.non.match <- function(expression_file, ribo.seqs) {
 # Extract names of ribosomal genes
+expression_file = read.table(expression_file, h=TRUE)
+ribo.seqs = read.fasta(ribo.seqs)
 ribo.names = attr(ribo.seqs, "name")
 
 # Compute quantile 
-quant = quantile(exp$xp, probs=seq(0.9,1,0.01))
+print(quantile(expression_file$xp, probs=seq(0.9,1,0.01)))
+quant = quantile(expression_file$xp, probs=seq(0.9,1,0.01))
 
-threshold = quant[9] # Take 98% quantile
+threshold = as.numeric(quant[9]) # Take 98% quantile
 
-high_exp = subset(exp, xp >= threshold)
+high_exp = subset(expression_file, xp >= threshold)
 
 # Return list of non ribosomal highly expressed genes
-matching = high_exp[!high_exp %in% as.factor(ribo.names)]
+matching = high_exp[!high_exp %in% as.factor(ribo.names), 1]
 
-write.table(matching, "results/highnonmatch.txt")
+write.table(matching, "results/highnonmatch.txt", quote=FALSE, row.names=FALSE, col.names=FALSE)
+}
