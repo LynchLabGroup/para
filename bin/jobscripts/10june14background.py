7f12cafeeb5ab2ab59755da18afdd9c03f625b38
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-10 16:18:11 -0400

    Added pdb scripting

--- bin/jobscripts/10june14background.py
+++ bin/jobscripts/10june14background.py
@@ -10,13 +10,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'gff'))
 
 import gff_func
 import parse_gff_v2 as pg
+import pdb
+gff = pg.load_gff("data/tetraurelia/tetraurelia51_EuGene_annotation.gff3", ["CDS","gene"])
+fasta = pg.load_fasta("data/tetraurelia/ptetraurelia_mac_51.fa")
 
-gff = pg.load_gff("../../data/tetraurelia/tetraurelia51_EuGene_annotation.gff3", ["CDS","gene"])
-fasta = pg.load_fasta("../../data/tetraurelia/ptetraurelia_assembly_v1.fa")
-
-with open("../../results/highnonmatch.txt", "r") as infile:
+with open("results/highnonmatch.txt", "r") as infile:
     records = [line.rstrip("\n") for line in infile.xreadlines()]
-
+pdb.set_trace()
 upstream_seqs = gff_func.retrieve_up(records, gff, fasta)[0]
 
-gff_func.write_fasta("../../results/ribo.background.fasta", upstream_seqs)
+gff_func.write_fasta("results/ribo.background.fasta", upstream_seqs)
