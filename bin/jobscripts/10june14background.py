ba9731fba3aa4517129bff3bb95eb16caec5f554
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-10 18:02:12 -0400

    Functionnalized script

--- bin/jobscripts/10june14background.py
+++ bin/jobscripts/10june14background.py
@@ -11,13 +11,35 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'gff'))
 import gff_func
 import parse_gff_v2 as pg
 
-gff = pg.load_gff("data/tetraurelia/tetraurelia51_EuGene_annotation.gff3", ["CDS","gene"])
-fasta = pg.load_fasta("data/tetraurelia/ptetraurelia_mac_51.fa")
 
-with open("results/highnonmatch.txt", "r") as infile:
-    records = [line.rstrip("\n") for line in infile.xreadlines()]
-upstream_seqs = gff_func.retrieve_up(records, gff, fasta, 250, 15)[0]
+def gene_names(inputname):
+    with open(inputname, "r") as infile:
+        records = [line.rstrip("\n") for line in infile.xreadlines()]
+    return records
 
-# Select only retrieved sequences
-upstream = [seq for seq in upstream_seqs if len(seq) > 1]
-gff_func.write_fasta("results/ribo.background.fasta", upstream)
+
+def background(inputname, outputname, gff, fasta):
+    """Take a list of gene name and extract their upstream regions using
+        given gff and fasta files.
+    """
+    records = gene_names(inputname)
+    upstream_seqs = gff_func.retrieve_up(records, gff, fasta, 250, 15)[0]
+
+    # Select only retrieved sequences
+    upstream = [seq for seq in upstream_seqs if len(seq) > 1]
+    return upstream
+
+
+def main():
+    gff = pg.load_gff("data/tetraurelia/tetraurelia51_EuGene_annotation.gff3",
+                      ["CDS", "gene"])
+    fasta = pg.load_fasta("data/tetraurelia/ptetraurelia_mac_51.fa")
+    inputname = "results/highnonmatch.txt"
+    outputname = "results/ribo.background.fasta"
+
+    upstream = background(inputname, outputname, gff, fasta)
+
+    gff_func.write_fasta(outputname, upstream)
+
+if __name__ == "__main__":
+    main()
