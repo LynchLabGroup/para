48dfeebfe8c6e24ad217309c9532871415e8fab9
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 17:52:33 -0400

    testfile now working perfectly

--- bin/tests/test_bigfoot.py
+++ bin/tests/test_bigfoot.py
@@ -10,29 +10,29 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bigfoot'))
 import weight
 
 
-class WeightSeq(unittest.TestCase):
+class TestWeight(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.seq = weight.WeightSeq("test\tATTTGC")
         self.gapped = weight.WeightSeq("gapped\tGCG-GGGCCC")
-        self.real = weight.WeightSeq("")
+        # self.real = weight.WeightSeq("")
 
     def test_individual_positions(self):
         self.assertEqual(self.seq[1], "A")
         self.assertEqual(self.seq[5], "G")
-        self.assertEqual(self.seq[len(self.seq)], "C")
+        self.assertEqual(self.seq[6], "C")
 
         with self.assertRaises(IndexError):
-            self.seq[len(self.seq)+1]
+            self.seq[7]
 
     def test_slices(self):
-        self.assertEqual(self.seq[1:3], "AT")
+        self.assertEqual(self.seq[1:3], "ATT")
         self.assertEqual(self.seq[1:], "ATTTGC")
-        self.assertEqual(self.seq[3:1], [])
+        self.assertEqual(self.seq[3:1], "")
 
     def test_rawseq(self):
-        self.assertEqual(self.seq.rawself.seq(), "ATTTGC")
-        self.assertEqual(self.gapped.rawself.seq(), "GCGGGGCCC")
+        self.assertEqual(self.seq.rawseq(), "ATTTGC")
+        self.assertEqual(self.gapped.rawseq(), "GCGGGGCCC")
 
     def test_name(self):
         self.assertEqual(self.seq.name(), "test")
