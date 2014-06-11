0500b91f3acedfd41be99d2c247b087de3b3829c
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 17:43:40 -0400

    Corrected syntax error

--- bin/tests/test_bigfoot.py
+++ bin/tests/test_bigfoot.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 # test bigfoot module
 import unittest
+import sys
+import os
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bigfoot'))
 
 import weight
@@ -9,32 +12,32 @@ import weight
 
 class WeightSeq(unittest.TestCase):
 
-    def setUp(self):
-        seq = weight.WeightSeq("test\tATTTGC")
-        gapped = weight.WeightSeq("gapped\tGCG-GGGCCC")
-        real = weight.WeightSeq("")
+    def setup(self):
+        self.seq = weight.WeightSeq("test\tATTTGC")
+        self.gapped = weight.WeightSeq("gapped\tGCG-GGGCCC")
+        self.real = weight.WeightSeq("")
 
     def test_individual_positions(self):
-        self.assertEqual(seq[1], "A")
-        self.assertEqual(seq[5], "G")
-        self.assertEqual(seq[len(seq), "C")
+        self.assertEqual(self.seq[1], "A")
+        self.assertEqual(self.seq[5], "G")
+        self.assertEqual(self.seq[len(self.seq)], "C")
 
         with self.assertRaises(IndexError):
-            seq[len(seq)+1]
+            self.seq[len(self.seq)+1]
 
     def test_slices(self):
-        self.assertEqual(seq[1:3], "AT")
-        self.assertEqual(seq[1:], "ATTTGC")
-        self.assertEqual(seq[3:1], [])
+        self.assertEqual(self.seq[1:3], "AT")
+        self.assertEqual(self.seq[1:], "ATTTGC")
+        self.assertEqual(self.seq[3:1], [])
 
     def test_rawseq(self):
-        self.assertEqual(seq.rawseq(), "ATTTGC")
-        self.assertEqual(gapped.rawseq(), "GCGGGGCCC")
+        self.assertEqual(self.seq.rawself.seq(), "ATTTGC")
+        self.assertEqual(self.gapped.rawself.seq(), "GCGGGGCCC")
 
     def test_name(self):
-        self.assertEqual(seq.name(), "test")
+        self.assertEqual(self.seq.name(), "test")
 
     def get_real_pos(self):
-        self.assertEqual(seq.get_real_pos(1), 1)
-        self.assertEqual(gapped.get_real_pos(4), 3)
+        self.assertEqual(self.seq.get_real_pos(1), 1)
+        self.assertEqual(self.gapped.get_real_pos(4), 3)
 
