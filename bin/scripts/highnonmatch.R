259e2ce3089a299e9a0e06de55ee2beac031af8f
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 14:39:18 -0400

    Reorganized code to be clearer

--- bin/scripts/highnonmatch.R
+++ bin/scripts/highnonmatch.R
@@ -6,15 +6,6 @@
 suppressPackageStartupMessages(library(seqinr))
 suppressPackageStartupMessages(library(argparse))
 
-# Command-line parser
-parser <- ArgumentParser(description="Script to extract the names highly expressed non ribosomal genes. Extract top expressed genes according to threshold, from expression file not matching sequences file.") 
-parser$add_argument("expfile", help="RNAseq expression tabfile, with a column \'xp\' and a column \'gene_id\' with gene name")
-parser$add_argument("sequences", help="Fasta file with the name of each gene as header")
-
-parser$add_argument("-t", "--thre", type="double",  default=0.98, help="Consider only genes over this percentile. Example: if t = 0.98, consider only top 2 percent highly expressed genes. (default %(default)s)")
-parser$add_argument("-o", "--output", default="highnonmatch.txt", help="name of the output file (default %(default)s)")
-
-args <- parser$parse_args()
 
 ### Functions ###
 high.non.match <- function(expression_file, ribo.seqs, quantile) {
@@ -36,11 +27,21 @@ matching = high_exp[!high_exp$gene_id %in% as.factor(ribo.names), 1]
 }
 
 
+### Command-line parser ###
+parser <- ArgumentParser(description="Script to extract the names highly expressed non ribosomal genes. Extract top expressed genes according to threshold, from expression file not matching sequences file.") 
+parser$add_argument("expfile", help="RNAseq expression tabfile, with a column \'xp\' and a column \'gene_id\' with gene name")
+parser$add_argument("sequences", help="Fasta file with the name of each gene as header")
+parser$add_argument("-t", "--thre", type="double",  default=0.98, help="Consider only genes over this percentile. Example: if t = 0.98, consider only top 2 percent highly expressed genes. (default %(default)s)")
+parser$add_argument("-o", "--output", default="highnonmatch.txt", help="name of the output file (default %(default)s)")
+args <- parser$parse_args()
 
-## Data loading
+
+### Data loading ###
 exp = read.table(args$expfile, h=TRUE)  # Expression data
 ribo.seqs = read.fasta(args$sequences)  # Ribosomal upstream sequences
 
+
+### Computing ###
 matching = high.non.match(exp, ribo.seqs, args$thre)
 print(paste("Writing file ", args$output))
 write.table(matching, args$output, quote=FALSE, row.names=FALSE, col.names=FALSE)
