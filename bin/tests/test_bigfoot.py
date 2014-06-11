b5d39f8e6dd9b11ea7a4e74934f6450cff854583
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 17:32:53 -0400

    Began to implement test for weight.py

--- bin/tests/test_bigfoot.py
+++ bin/tests/test_bigfoot.py
@@ -2,8 +2,32 @@
 # -*- coding: utf-8 -*-
 # test bigfoot module
 import unittest
-from .. import bigfoot
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bigfoot'))
+
+import weight
+
 
 class WeightSeq(unittest.TestCase):
 
-    def test_
+    def setUp(self):
+        seq = weight.WeightSeq("test\tATTTGC")
+    def test_individual_positions(self):
+        self.assertEqual(seq[1], "A")
+        self.assertEqual(seq[5], "G")
+        self.assertEqual(seq[len(seq), "C")
+
+        with self.assertRaises(IndexError):
+            seq[len(seq)+1]
+
+    def test_slices(self):
+        self.assertEqual(seq[1:3], "AT")
+        self.assertEqual(seq[1:], "ATTTGC")
+        self.assertEqual(seq[3:1], [])
+
+    def test_rawseq(self):
+        self.assertEqual(seq.rawseq(), "ATTTGC")
+
+    def test_name(self):
+        self.assertEqual(seq.name(), "test")
+        
+
