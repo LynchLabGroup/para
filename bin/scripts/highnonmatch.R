bf74f98ed9e300df6af85bae425f32604d96948d
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 14:52:18 -0400

    Modified quotes type

--- bin/scripts/highnonmatch.R
+++ bin/scripts/highnonmatch.R
@@ -28,7 +28,7 @@ matching = high_exp[!high_exp$gene_id %in% as.factor(ribo.names), 1]
 
 
 ### Command-line parser ###
-parser <- ArgumentParser(description="Script to extract the names highly expressed non ribosomal genes. Extract top expressed genes according to threshold, from expression file not matching sequences file.") 
+parser <- ArgumentParser(description='Script to extract the names highly expressed non ribosomal genes. Extract top expressed genes according to threshold, from expression file not matching sequences file.') 
 parser$add_argument("expfile", help="RNAseq expression tabfile, with a column \'xp\' and a column \'gene_id\' with gene name")
 parser$add_argument("sequences", help="Fasta file with the name of each gene as header")
 parser$add_argument("-t", "--thre", type="double",  default=0.98, help="Consider only genes over this percentile. Example: if t = 0.98, consider only top 2 percent highly expressed genes. (default %(default)s)")
