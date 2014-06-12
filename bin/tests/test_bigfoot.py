efd7ec27aff8667eae24bf32ccd2249267312889
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-11 21:15:21 -0400

    Added test for weight function

--- bin/tests/test_bigfoot.py
+++ bin/tests/test_bigfoot.py
@@ -9,16 +9,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bigfoot'))
 
 import weight
 import mock
+from StringIO import StringIO
 
 
 class TestWeight(unittest.TestCase):
     def setUp(self):
         self.seq = weight.WeightSeq("test\tATTTGC")
         self.gapped = weight.WeightSeq("gapped\tgcg-gggccc----atgcggg")
-        self.files = mock.Mock({"predfile": "0.0\n0.95\n1.0\n1.0\n0.8\n0.95\n1.0\n1.0\n\
-            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0",
-                                "mpdscore": "0.0\n0.8\n0.86\n0.9\n0.9\n0.95\n1.0\n1.0\n\
-            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0"})
+        self.files = mock.Mock({"pred": StringIO("0.0\n0.95\n1.0\n1.0\n0.8\n0.95\n1.0\n1.0\n\
+            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0\n0.0\n0.9\n0.9\n1.0\n1.0"),
+                                "mpd": StringIO("1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n\
+            1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n0.0\n0.0\n0.0\n0.0\n0.0\n0.0")})
 
     def test_individual_positions(self):
         self.assertEqual(self.seq[1], "A")
@@ -28,7 +29,7 @@ class TestWeight(unittest.TestCase):
         with self.assertRaises(IndexError):
             self.seq[7]
 
-    def test_slices(self):
+    def test_get_slices(self):
         self.assertEqual(self.seq[1:3], "ATT")
         self.assertEqual(self.seq[1:], "ATTTGC")
         self.assertEqual(self.seq[3:1], "")
@@ -40,6 +41,18 @@ class TestWeight(unittest.TestCase):
     def test_name(self):
         self.assertEqual(self.seq.name(), "test")
 
-    def get_real_pos(self):
+    def test_get_real_pos(self):
         self.assertEqual(self.seq.get_real_pos(1), 1)
         self.assertEqual(self.gapped.get_real_pos(4), 3)
+
+    def test_weight(self):
+        self.seq.weight(self.files.mpd, self.files.pred)
+        real_pred = [0.0, 0.95, 1.0, 1.0, 0.8, 0.95, 1.0, 1.0,
+                     0.95, 0.9, 0.0, 0.0, 0.9, 0.9, 1.0, 1.0, 0.0, 0.9, 0.9,
+                     1.0, 1.0]
+
+        real_mpd = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                    0.0]
+        self.assertEqual([base[1] for base in self.seq._w], real_pred)
+        self.assertEqual([base[2] for base in self.seq._w], real_mpd)
