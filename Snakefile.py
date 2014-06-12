4d97e7165e1226c768c9db65ef8373a95772536e
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-12 16:51:24 -0400

    Profoundly modified inputs and outputs paths

--- Snakefile.py
+++ Snakefile.py
@@ -7,7 +7,7 @@ RES = [os.path.splitext(famfile)[0]+".comp" for famfile in glob.glob("results/WG
 BASENAME = [os.path.basename(family).rstrip(".comp") for family in RES]
 UPSTREAM = "data/families/WGD2/upstream"
 CDSLOC = "data/families/WGD2/CDS/nt"
-RESULTS = "results"
+RESULTS = "results/"
 MINLEN = 15
 MAXLEN = 250
 MINFAMMEMBER = 4
@@ -53,7 +53,7 @@ rule make_dir:
 rule edit_upstream:
     """Edit upstream sequences and move them into results folder."""
     input: UPSTREAM+"/{family}.fasta"
-    output: RESULTS+"/{family}/{family}.fasta"
+    output: RESULTS+"{family}/{family}.fasta"
     shell: """
     python2 bin/scripts/fastaheader.py {input[0]} '|' {output}
     """
@@ -61,65 +61,65 @@ rule edit_upstream:
 ### Operations on CDSs
 rule align_CDS:
     """Aligning CDSs"""
-    input: "{family}.fasta", "{family}.CDS.fasta"
-    params: prefix="{family}.CDS"
-    output: "{family}.CDS.nt_ali.fasta"
+    input: CDSLOC+"/{family}.fasta"
+    params: prefix=RESULTS+"{family}/{family}.CDS"
+    output: RESULTS+"{family}/{family}.CDS.nt_ali.fasta"
     shell: "perl bin/scripts/translatorx_vLocal.pl -c 6 -i {input} -o {params.prefix}"
 
 rule transform_CDS_header:
     """Reduce CDS header."""
-    input: "{family}.CDS.nt_ali.fasta"
-    output: "{family}.CDS.fasta"
+    input: RESULTS+"{family}/{family}.CDS.nt_ali.fasta"
+    output: RESULTS+"{family}/{family}.CDS.fasta"
     shell: "python2 bin/scripts/fastaheader.py {input} '|' {output}"
 
 rule match_seqs:
     """Matching sequences between CDS and family."""
-    input: "{family}.fasta", "{family}.CDS.fasta"
-    output: "{family}.CDS.e.fasta"
+    input: expand("{dir}{{family}}/{{family}}.{end}", dir=RESULTS, end=["fasta", "CDS.fasta"])
+    output: RESULTS+"{family}/{family}.CDS.e.fasta"
     shell: "python2 bin/scripts/extractmatch.py {input} {output}"
 
 rule fasta_to_phylip:
     """Convert Fasta to Phylip sequences."""
-    input: "{family}.CDS.e.fasta"
-    output: "{family}.CDS.phyl"
+    input: RESULTS+"{family}/{family}.CDS.e.fasta"
+    output: RESULTS+"{family}/{family}.CDS.phyl"
     shell: "perl bin/scripts/ConvertFastatoPhylip.pl {input} {output}"
 
 rule phyml_tree:
     """Compute ML gene tree."""
-    input: "{family}.CDS.phyl"
-    output: "{family}.CDS.phyl_phyml_tree"
+    input: RESULTS+"{family}/{family}.CDS.phyl"
+    output: RESULTS+"{family}/{family}.CDS.phyl_phyml_tree"
     shell: "phyml -i {input}"
 
 rule gene_tree:
     """Create a new tree to be compatible with BigFoot"""
-    input: "{family}.CDS.phyl_phyml_tree.txt"
-    output: "{family}.CDS.newick"
+    input: RESULTS+"{family}/{family}.CDS.phyl_phyml_tree.txt"
+    output: RESULTS+"{family}/{family}.CDS.newick"
     shell: "python2 bin/scripts/editnewick.py {input} {output}"
 
 
 ### Motif finding
 rule bigfoot:
     """Use BigFoot on given files"""
-    input: "{family}.fasta", "{family}.newick"
-    output: "{family}.fasta.mpd", "{family}.fasta.pred"
+    input: expand("{dir}{{family}}/{{family}}.{end}", dir=RESULTS, end=["fasta", "newick"])
+    output: expand("{dir}{{family}}/{{family}}.{end}", dir=RESULTS, end=["mpd", "pred"])
     shell: "java -jar ../BigFoot/BigFoot.jar -t {input[1]} -p={BFPARAM} {input[0]}"
 
 rule meme_motifs:
     """Find motifs in sequence with MEME."""
-    input: "{family}.fasta"
-    output: "{family}.meme.motifs"
+    input: RESULTS+"{family}/{family}.fasta"
+    output: RESULTS+"{family}/{family}.meme.motifs"
     shell: "meme -minw {MINWIDTH} -nmotifs {NMOTIFS} -evt {EVAL} -dna -text {input} >> {output}"
 
 rule bigfoot_motifs:
     """Parse BigFoot's outputs to produce .motifs files"""
-    input:  "{family}.fasta.mpd", "{family}.fasta.pred"
-    output: "{family}.motifs"
+    input:  expand("{dir}{{family}}/{{family}}.{end}", dir=RESULTS, end=["mpd", "pred"])
+    output: RESULTS+"{family}/{family}.motifs"
     shell: "python2 bin/bigfoot/setup.py {input} -o {output} -t {PREDTHRE} -a {ALITHRE}"
 
 rule comp:
     """Rule to produce motifs comparison between Bigfoot and MEME motifs"""
-    input: "{family}.motifs", "{family}.meme.motifs"
-    output: "{family}.comp"
+    input: expand("{dir}{{family}}/{{family}}.{end}", dir=RESULTS, end=["motifs", "meme.motifs"])
+    output: RESULTS+"{family}/{family}.comp"
     shell: "python2 bin/bigfoot/memecomp.py {input} -t {OVERLAP} -e {EVAL} -o {output}"
 
 ### General Rules ###
