ac37bdb4f39060d0d0c1e5a3b54ddb174d2b91df
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-13 14:19:14 -0400

    Added functionnal test for weight operation

--- bin/tests/test_bigfoot.py
+++ bin/tests/test_bigfoot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # test bigfoot module
-import unittest
 import sys
 import os
 
@@ -9,6 +8,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bigfoot'))
 
 import weight
 import mock
+import unittest
+import tempfile
 from StringIO import StringIO
 
 
@@ -16,7 +17,7 @@ class TestWeight(unittest.TestCase):
     def setUp(self):
         self.seq = weight.WeightSeq("test\tATTTGC")
         self.gapped = weight.WeightSeq("gapped\tgcg-gggccc----atgcggg")
-        
+
     def test_individual_positions(self):
         self.assertEqual(self.seq[1], "A")
         self.assertEqual(self.seq[5], "G")
@@ -41,24 +42,36 @@ class TestWeight(unittest.TestCase):
         self.assertEqual(self.seq.get_real_pos(1), 1)
         self.assertEqual(self.gapped.get_real_pos(4), 3)
 
-    def test_weight(self):
+    def test_weight_pred(self):
         mpd = "1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n\
             1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n0.0\n0.0\n0.0\n0.0\n0.0\n0.0"
         pred = "0.0\n0.95\n1.0\n1.0\n0.8\n0.95\n1.0\n1.0\n\
-            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0\n0.0\n0.9\n0.9\n1.0\n1.0"
-        # print files.mpd
-        with mock.patch('__builtin__.open', mock.mock_open(read_data=mpd)) as m:
-            with mock.patch('__builtin__.open', mock.mock_open(read_data=pred)) as p:
-                self.seq.weight(m, p)
-
-        real_pred = [0.0, 0.95, 1.0, 1.0, 0.8, 0.95, 1.0, 1.0,
-                     0.95, 0.9, 0.0, 0.0, 0.9, 0.9, 1.0, 1.0, 0.0, 0.9, 0.9,
+            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0\n"
+        self.file_mpd = tempfile.NamedTemporaryFile(delete=False)
+        self.file_mpd.write(mpd)
+        self.file_mpd.close()
+        self.file_pred = tempfile.NamedTemporaryFile(delete=False)
+        self.file_pred.write(pred)
+        self.file_pred.close()
+        self.gapped.weight(self.file_mpd.name, self.file_pred.name)
+        real_pred = [0.0, 0.95, 1.0, 0.0, 1.0, 0.8, 0.95, 1.0, 1.0,
+                     0.95, 0.0, 0.0, 0.0, 0.0, 0.9, 0.0, 0.0, 0.9, 0.9,
                      1.0, 1.0]
+        self.assertEqual([base[1] for base in self.gapped._w], real_pred)
 
+    def test_weight_mpd(self):
+        mpd = "1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n\
+            1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n1.0\n0.0\n0.0\n0.0\n0.0\n0.0\n0.0"
+        pred = "0.0\n0.95\n1.0\n1.0\n0.8\n0.95\n1.0\n1.0\n\
+            0.95\n0.9\n0.0\n0.0\n0.9\n0.9\n1.0\n1.0\n"
+        self.file_mpd = tempfile.NamedTemporaryFile(delete=False)
+        self.file_mpd.write(mpd)
+        self.file_mpd.close()
+        self.file_pred = tempfile.NamedTemporaryFile(delete=False)
+        self.file_pred.write(pred)
+        self.file_pred.close()
+        self.gapped.weight(self.file_mpd.name, self.file_pred.name)
         real_mpd = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                     1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                     0.0]
-        print real_pred
-        print [base[1] for base in self.seq._w]
-        self.assertEqual([base[1] for base in self.seq._w], real_pred)
-        self.assertEqual([base[2] for base in self.seq._w], real_mpd)
+        self.assertEqual([base[2] for base in self.gapped._w], real_mpd)
