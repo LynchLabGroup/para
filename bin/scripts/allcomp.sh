1ff25546ebcb002490c760faad53e5775d087a7d
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-30 16:51:41 -0400

    Add CLI with output file name

--- bin/scripts/allcomp.sh
+++ bin/scripts/allcomp.sh
@@ -1,1 +1,6 @@
- results/WGD2ANC0*/*comp | xargs wc -l | awk '{if(!/total/ && $1 > 0) print $2}' | xargs awk FNR-1 > results/15may14consensuscomp.txt
+# Allcomp.sh
+# Produces a list with all motifs in .comp files
+# USAGE:
+# bin/allcomp.sh output
+output=$1
+results/WGD2ANC0*/*comp | xargs wc -l | awk '{if(!/total/ && $1 > 0) print $2}' | xargs awk FNR-1 > $output
