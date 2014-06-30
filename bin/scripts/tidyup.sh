cff9a7b17858e3e568905a6b7744f84d0dd6f1d7
Rekyt <matthias.grenie@ens-lyon.fr>
2014-06-30 15:46:44 -0400

    Modified script to accept a list of files

--- bin/scripts/tidyup.sh
+++ bin/scripts/tidyup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Tidy up folder after translatorX script
 # USAGE
-# tidyup targetlocation
+# tidyup file_list
 
 d=$(date "+%Y-%m-%d") #Stores date value into variable
 echo $d
 
-TARGET=$1
-
+for TARGET in $1;
+do
 find $TARGET -name "*.nt[1-3]*_ali.fasta" -delete
 find $TARGET -name "*.aa*" -delete
 find $TARGET -name "*.html" -delete
 find $TARGET -name "*.muscle.log" -delete
+done
